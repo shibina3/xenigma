8ef9e4fb8b3a629aaa84f62c64398809d7bf4537
shibina3 <shibinashibi1507@gmail.com>
2024-05-10 02:02:24 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, HashRouter as Router } from 'react-router-dom';
-import router from './router';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    
+    <App />
   </Router>
 );
 
