6f1d1e7dfe846adb58cfcd74191cc67b0d79552e
Shibina <shibinashibi1507@gmail.com>
2024-05-25 03:26:09 +0530

    generic share functionality added

--- src/components/URLContainer.js
+++ src/components/URLContainer.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import { FaHandPointDown, FaRegCopy, FaShareSquare, FaWhatsapp, FaTelegramPlane } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { PiMicrosoftTeamsLogo } from "react-icons/pi";
 
 export default function URLContainer({ url, title, onCopyURL, copyBtnText, content, contentTitle }) {
+    const [loading, setLoading] = useState(false);
+
+    const genericShare = async () => {
+        setLoading(true);
+        await navigator.share({
+            title: contentTitle,
+            text: content
+        })
+        setLoading(false);
+    };
+
     return (
         <>
             <h6 className='text-center fs-6'>{title} <FaHandPointDown className='color-yellow' /></h6>
@@ -19,26 +30,32 @@ export default function URLContainer({ url, title, onCopyURL, copyBtnText, conte
                     >
                         <span className='cursor-pointer' onClick={onCopyURL} ><FaRegCopy /></span>
                     </OverlayTrigger>
-                    <OverlayTrigger
-                        placement="top"
-                        trigger="click"
-                        rootClose={true}
-                        overlay={
-                            <Tooltip id='tooltip-top' className='white-tooltip'>
-                                <div className='d-flex'>
-                                    <span className='fw-500'>Share :</span>
-                                    <span className='social-sharing d-flex ms-2 align-items-center gap-2'>
-                                        <FaWhatsapp onClick={() => window.open(`https://wa.me/?text=${encodeURIComponent(contentTitle + ': ' + content)}`,'_blank')} className='whatsapp cursor-pointer' />
-                                        <FaTelegramPlane onClick={() => window.open(`https://telegram.me/share/url?url=${contentTitle}&text=${encodeURIComponent(content)}`, '_blank')} className='telegram cursor-pointer' />
-                                        <PiMicrosoftTeamsLogo onClick={() => window.open(`https://teams.microsoft.com/l/message/create?message=${encodeURIComponent(contentTitle + ': ' + content)}`,'_blank')} className='teams cursor-pointer d-none' />
-                                        <MdOutlineEmail onClick={() => window.open(`mailto:?subject=${encodeURIComponent(contentTitle)}&body=${encodeURIComponent(content)}`,'_blank')} className='mail cursor-pointer' />
-                                    </span>
-                                </div>
-                            </Tooltip>
-                        }
-                    >
-                        <span className='cursor-pointer'><FaShareSquare /></span>
-                    </OverlayTrigger>
+                    {
+                        navigator.share ? <span className='cursor-pointer' onClick={genericShare}><FaShareSquare />{
+                            loading ? <div id="loading-bar-spinner" className="spinner"><div className="spinner-icon"></div></div> : null
+                        }</span>
+                            :
+                            <OverlayTrigger
+                                placement="top"
+                                trigger="click"
+                                rootClose={true}
+                                overlay={
+                                    <Tooltip id='tooltip-top' className='white-tooltip'>
+                                        <div className='d-flex'>
+                                            <span className='fw-500'>Share :</span>
+                                            <span className='social-sharing d-flex ms-2 align-items-center gap-2'>
+                                                <FaWhatsapp onClick={() => window.open(`https://wa.me/?text=${encodeURIComponent(contentTitle + ': ' + content)}`, '_blank')} className='whatsapp cursor-pointer' />
+                                                <FaTelegramPlane onClick={() => window.open(`https://telegram.me/share/url?url=${contentTitle}&text=${encodeURIComponent(content)}`, '_blank')} className='telegram cursor-pointer' />
+                                                <PiMicrosoftTeamsLogo onClick={() => window.open(`https://teams.microsoft.com/share?msgText=${encodeURIComponent(contentTitle + ': ' + content)}&preview=false&s=1675706704556`, '_blank')} className='teams cursor-pointer' />
+                                                <MdOutlineEmail onClick={() => window.open(`mailto:?subject=${encodeURIComponent(contentTitle)}&body=${encodeURIComponent(content)}`, '_blank')} className='mail cursor-pointer' />
+                                            </span>
+                                        </div>
+                                    </Tooltip>
+                                }
+                            >
+                                <span className='cursor-pointer'><FaShareSquare /></span>
+                            </OverlayTrigger>
+                    }
                 </div>
             </div>
         </>
